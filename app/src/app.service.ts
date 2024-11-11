840a5e20f9ac98e4cff556b44c5399b4ae364208
nguyenngocdue <duengocnguyen@gmail.com>
2024-11-11 20:46:28 +0700

    minor

--- app/src/app.service.ts
+++ app/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'What time is it ?';
+    return 'What time is it ? My name is Due';
   }
 }
